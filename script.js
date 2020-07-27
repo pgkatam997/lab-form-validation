5c45f125c40164e3fc36febf30bc117116ed8e54
Purnima G Katam (S-59) <68459602+pgkatam997@users.noreply.github.com>
2020-07-27 14:46:40 +0530

    done

--- script.js
+++ script.js
@@ -8,6 +8,36 @@
 // username should not contain special characters
 // the function should return true if all the conditions above are validated.
 // return false if the validation fails
+function checkUserName(){
+    let username =document.getElementById("usertext");
+    if(username.value.trim==""){
+        alert("username should not be empty");
+        return false;
+    }
+    else if(username.value.trim().length < 8)
+    {
+        alert("username should be a minimum of 8 characters and above ");
+        return false;
+    }
+    else if(username.value.trim().length > 8){
+        alert("username should be less than 15 characters");
+    }
+    let username =document.getElementById("usertext").value;
+    if(/["/^\s+$/"]/.test(username))
+    {
+        alert("No space is allowed in username");        
+        return false;
+    }
+    else if(/[^a-zA-Z0-9]/.test(username))
+    {        
+        alert("username should not contain special characters");
+        return false;
+    }
+    else{
+        return true;
+    }
+}
+
 
 // Progression 2 
 // Check mail
@@ -15,6 +45,17 @@
 // only @ , . , _ [at,dot,underscore] is allowed
 // It can be alphanumeric in nature
 // return true if it passses all validation and false otherwise
+function checkEmail(){
+    let email=document.getElementById("emailtext");
+        if(/[\w+@.]/.test(email)){
+            alert('should not contain special character');
+            return false;
+        }
+        else{
+            return true;
+        }  
+}
+
 
 // Progression 3
 // Check password
@@ -22,3 +63,17 @@
 // should be alphanumeric in nature
 // password must contain atleast one Uppercase, one number and special characters[!@#$%^&*()_]
 // return true else return false
+function checkPassword(){
+    let password=document.getElementById("passwordtext");
+    if(/[^a-zA-Z0-9]/.test(password)){
+        alert('should be alphanumeric in nature');
+        return false;
+    }
+    else if(/[!@#$%^&*()_]/.test(password)){
+        alert('password must contain uppercase, one number, and one special charater');
+        return false;
+    }
+    else{
+        return true;
+    }
+}
